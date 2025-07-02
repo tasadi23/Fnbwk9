5790444a0722ef985df315c54747d9712c64f914
Tasneem Mahomed <tasadiventures@gmail.com>
2025-07-02 14:19:49 +0200

    changes

---
+++ rectangle_calc.py/calculate.py
@@ -0,0 +1,8 @@
+
+def area(length, width):
+    #Function to calculate the area
+    return length * width
+
+def perimeter(length,width):
+    #fuction to calculate the perimeter
+    return 2 * (length + width)
