a47744d1a032af8aa078e9dff7f52a5176df5660
Tasneem Mahomed <tasadiventures@gmail.com>
2025-07-02 14:26:33 +0200

    changes

--- exception_handling.py
+++ exception_handling.py
@@ -1,3 +1,19 @@
+#try:
+#print(x)
+    #except NameError:
+#print("Variable x is not defined")
+    #except:
+#print("An exception occurred")
+
+#try:
+#print (x)
+    #except NameError:
+#print("Variable x is not defined")
+    #else:
+#print("Everything went wrong")
+#x = -1
+    #if x < 0:
+    #raise Exception("Sorry, no numbers below zero"?/
 
 try:
     print(x)
