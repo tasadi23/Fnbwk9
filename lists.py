866622a9e3fabd0ea2533fd6f37b1db2ab7f9e7e
Tasneem Mahomed <tasadiventures@gmail.com>
2025-07-06 13:28:13 +0200

    changes

---
+++ lists.py
@@ -0,0 +1,13 @@
+fruits = ["apple", "banana", "cherry"]
+
+fruits.append ("kiwi")
+print(fruits)
+
+fruits.insert(1, "orange")
+print(fruits)
+
+fruits.remove("kiwi")
+print(fruits)
+
+fruits.sort()
+print (fruits)
